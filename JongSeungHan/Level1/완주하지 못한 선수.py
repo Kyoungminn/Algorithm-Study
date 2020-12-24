8094687300a377a38ab0c1769e0148900de5bd96
BellWin98 <jongseunghan@naver.com>
2020-12-24 15:22:58 +0900

    solved, use collections.Counter module

--- JongSeungHan/Level1/완주하지 못한 선수.py
+++ JongSeungHan/Level1/완주하지 못한 선수.py
@@ -1,20 +1,18 @@
+from collections import Counter
+
 def solution(participant, completion):
     answer = ''
+    result = (Counter(participant) - Counter(completion)) # Counter 모듈은 객체간 가감이 가능, dict 형식으로 중복의 개수 카운트.
+    answer = list(result)[0] # list의 0번째 요소 추출
 
-    for p in participant:
-        if p in completion:
-            completion.remove(p)
-        else:
-            answer = p
-    
     return answer
 
 # participant = ["leo", "kiki", "eden"]
 # completion = ["eden", "kiki"]
-participant = ["marina", "josipa", "nikola", "vinko", "filipa"]
-completion = ["josipa", "filipa", "marina", "nikola"]
-# participant = ["mislav", "stanko", "mislav", "ana"]
-# completion = ["stanko", "ana", "mislav"]
+# participant = ["marina", "josipa", "nikola", "vinko", "filipa"]
+# completion = ["josipa", "filipa", "marina", "nikola"]
+participant = ["mislav", "stanko", "mislav", "ana"]
+completion = ["stanko", "ana", "mislav"]
 
 print(solution(participant, completion))
 
